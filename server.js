270a31b504d20662bc149b2db40a75252c660910
CodeGeta <akshaykatira294@gmail.com>
2020-05-17 13:54:48 -0400

    Server now knows when a user connects

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false }))
+
 
 
 var messages = [
@@ -19,14 +22,17 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body)
-    console.log(req.body)
+    io.emit('message', req.body)
+    // console.log(req.body)
     res.sendStatus(200)
 })
 
 
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
 
-
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log('Server is listening on port ', server.address().port)
 })
 
