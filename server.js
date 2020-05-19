5fe53c5f9da8d669d13154e70966eaf1cf544f5f
CodeGeta <akshaykatira294@gmail.com>
2020-05-19 15:59:38 -0400

    Connected to a mongo database

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+var db = require('./config')
+dbUrl = db.dbUrl
+
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false }))
+
+
+
+
+var Message = mongoose.model('Message', {
+    name : String,
+    message : String
+})
+
 
 
 
@@ -17,20 +31,40 @@ var messages = [
 
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+    // res.send(messages)
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    // console.log(req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if(err) {
+            sendStatus(500)
+        }
+
+        // messages.push(req.body)
+        io.emit('message', req.body)
+        // console.log(req.body)
+        res.sendStatus(200)
+    })
+
+
 })
 
 
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
+
+
+
+mongoose.connect(dbUrl, {useNewUrlParser: true,  useUnifiedTopology: true },(err) => {
+    console.log("mongo db connection", err)
+})
+
 
 var server = http.listen(3000, () => {
     console.log('Server is listening on port ', server.address().port)
